64399b602fa8c8ff0b2aa13f5d5ada9a3b95df1b
jasiwi09 <jasiwi09@gmail.com>
2024-12-10 00:24:16 +0100

    Contact update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,13 +6,14 @@ export default function Contact() {
 
     const [message, setMessage] = useState("");
 
-
     function handleSendButton(event) {
         event.preventDefault();
 
         const emailInput = document.querySelector('input[type="email"]');
-        if (!emailInput.value) {
-            setMessage(<span style={{color: 'red'}}>Falta por introducir el email.</span>);
+        const nameInput = document.querySelector('input[type="text"]');
+        const messageInput = document.querySelector('textarea');
+        if (!emailInput.value || !nameInput.value || !messageInput.value) {
+            setMessage(<span style={{color: 'red'}}>Falta por introducir algún campo.</span>);
             return;
         } else if (!emailInput.value.includes('@')) {
             setMessage(<span style={{color: 'red'}}>El email introducido no es válido.</span>);
@@ -20,6 +21,7 @@ export default function Contact() {
         }
 
         setMessage(<span style={{color: 'lime', textAlign: 'center'}}>¡Email recibido! Pronto contactaré contigo 😀</span>);
+        
     }
 
     return (
@@ -27,7 +29,13 @@ export default function Contact() {
             <form action="">
                 <h2>¿Tienes algún proyecto en mente?</h2>
                 <h3>¡Contacta conmigo!</h3>
+                <label htmlFor="email">Email</label>
                 <input type="email" placeholder="Introduce tu email" required />
+                <label htmlFor="name">Nombre completo</label>
+                <input type="text" placeholder="Introduce tu nombre" name='name' required />
+                <label htmlFor="name">Mensaje</label>
+                <textarea name="name" id="name" placeholder='Introduce el mensaje'></textarea>
+
                 <button type="submit" onClick={handleSendButton}>Enviar</button>
                 {message}
             </form>
